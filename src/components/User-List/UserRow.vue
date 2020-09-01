ce5605348441ae59acd59d9bb48be2f932645c91
betooliveira18 <beto.oliveirassa@gmail.com>
2020-09-01 19:48:22 -0300

    user list done

---
+++ src/components/User-List/UserRow.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="userRow">
+    <div class="avatar" :class="{'bot-avatar' : isBot}"></div>
+    <strong>{{ nickName }}</strong>
+    <div class="bot" v-if="isBot">Bot</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isBot: Boolean,
+    nickName: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.userRow {
+  display: flex;
+  margin-top: 5px;
+  padding: 5px;
+  align-items: center;
+  cursor: pointer;
+  background: transparent;
+  transition: 0.2s;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.1);
+
+  }
+  strong {
+    margin-left: 13px;
+    font-weight: 500;
+    color: var(--white);
+    opacity: .7;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+
+  }
+
+  .avatar {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    flex-shrink: 0;
+    background: var(--primary);
+  &.bot-avatar {
+      background: var(--mention-detail);
+    }
+
+  }
+
+  .bot{
+    margin-left: 9px;
+    background-color: var(--discord);
+    color: var(--white);
+    border-radius: 4px;
+    padding: 4px 5px;
+    text-transform: uppercase;
+    font-size: 11px;
+    font-weight: bold;
+
+
+  }
+  }
+
+</style>
--- src/components/User-List/index.vue
+++ src/components/User-List/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="userList">
-    <div class="offline"></div>
-    <div class="online"></div>
+    <div class="role">Disponivel - 2</div>
+    <UserRow nickName="Ediberto Oliveira" />
+    <UserRow nickName="MecBOT" isBot/>
+    <div class="role">Indisponivel - 18</div>
+    <UserRow nickName="Fulano de tal" v-for="user in 20" :key="user.id"/>
   </div>
 </template>
 
 <script>
+import UserRow from  './UserRow';
 export default {
-
-}
+  components: {
+    UserRow,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -33,5 +39,12 @@ export default {
     background: var(--secondary);
   }
 
+  }
+  .role {
+    margin-top: 20px;
+    color: var(--grey);
+    font-size: 12px;
+    font-weight: 500;
+    text-transform: uppercase;
   }
 </style>
